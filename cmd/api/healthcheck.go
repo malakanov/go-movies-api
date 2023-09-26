cb7f72d33829f0cbc5fd5367e88c7a88f6b0d875
malakanov <malakanov@alkor.co.ru>
2023-09-26 09:56:58 +0500

    add json marshal

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
-	_, err := fmt.Fprintln(w, "status: available")
-	if err != nil {
-		app.logger.Println(err)
+
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
 	}
 
-	_, err = fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	js, err := json.Marshal(data)
+
 	if err != nil {
 		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
 	}
 
-	_, err = fmt.Fprintf(w, "version: %s\n", version)
-	if err != nil {
-		app.logger.Println(err)
-	}
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
